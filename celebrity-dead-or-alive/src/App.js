280564cb1cb251ba64952d32ac9c740cd8e41e0c
Alexis <reche06@gmail.com>
2020-01-05 21:55:24 -0500

    home page is done / with Nav Bar

---
+++ celebrity-dead-or-alive/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import NavBar from './NavBar';
+import WelcomePage from './WelcomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <WelcomePage/>
+   
+    </div>
+  );
+}
+
+export default App;
