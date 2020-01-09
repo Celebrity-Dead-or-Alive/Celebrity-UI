782197ca6e8f393453250f1da6ae4d362a230841
Zealll <eriznis@gmail.com>
2020-01-08 20:30:23 -0500

    Fixed the timer

--- celebrity-dead-or-alive/src/Components/Timebar.js
+++ celebrity-dead-or-alive/src/Components/Timebar.js
@@ -7,41 +7,39 @@ import './Timer.css';
 const TimeBar = () => {
     const [seconds, setSeconds] = useState("00:00");
     const [isActive, setIsActive] = useState(true);
-    // const[toggle, setToggle]=useState(true);
-
 
     useEffect(() => {
-      let interval = null;
-      console.log("useEffect")
-      if (isActive) {
-          console.log("active");
-          
-          let firstNum=seconds[0]
-          let secNum=seconds[1]
-          let thirdNum=seconds[3]
-          let fourthNum=seconds[4]
-          fourthNum=Number(fourthNum)+1
-        if (fourthNum >9){
-            fourthNum="0"
-            thirdNum=Number(thirdNum)+1
-        }
-        else if (thirdNum == 5 && fourthNum==0){
-            setTimeout(()=>{
-                 thirdNum="0"
-                 fourthNum="0"
-                secNum=Number(secNum + 1) 
-            }, 1000)
-          
-        }
+      if (isActive) {          
+          let firstNum = seconds[0]
+          let secNum = seconds[1]
+          let thirdNum = seconds[3]
+          let fourthNum = seconds[4]
+          fourthNum = Number(fourthNum)+1
+
           setTimeout(()=>{
-              console.log("running");
+            if(Number(firstNum) === 6) {
+              window.alert('Time is up!')
+              setIsActive(false)
+              fourthNum = '0'
+            } else if(Number(secNum) === 9 && Number(thirdNum) > 4 && Number(fourthNum) > 9) {
+              firstNum = Number(firstNum) + 1
+              secNum = '0'
+              thirdNum = '0'
+              fourthNum = '0'
+            } else if(Number(thirdNum) > 4 && Number(fourthNum) > 9) {
+              fourthNum = '0'
+              thirdNum = '0'
+              secNum = Number(secNum) + 1
+            } else if(fourthNum > 9) {
+              fourthNum = '0'
+              thirdNum = Number(thirdNum) + 1
+            } 
             setSeconds(`${firstNum+secNum}:${thirdNum+fourthNum}`)
-            // setToggle(!toggle)
+            
         }, 1000)
           
         }
-     
-    }, [seconds])
+    }, [ seconds ])
   
     return (
       <div className="app">
